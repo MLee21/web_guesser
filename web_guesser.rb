c85a411651acef8c5fd9677c9ce0ff66f96ea0bf
MLee21 <minnie.lee89@gmail.com>
2015-03-23 14:43:45 -0600

    completed web guesser. Made sure colors weren't too painful.

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,21 +4,27 @@ require 'sinatra/reloader'
 NUMBER = rand(100)
 
 get '/' do
+  @background = "white"
   message = guess_checker(params["guess"])
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  erb :index, :locals => {:number => NUMBER, :message => message, :background => @background}
 end
 
 def guess_checker(guess)
   case 
   when guess.to_i >= (NUMBER + 5)
+     @background = "red"
     "Way too high!"
   when guess.to_i <= (NUMBER - 5)
+    @background = "red"
     "Way too low!"
   when NUMBER < guess.to_i && guess.to_i < (NUMBER + 5)
+    @background = "indianred"
     "Too high!"
   when NUMBER > guess.to_i && guess.to_i > (NUMBER - 5)
+     @background = "lightblue"
      "Too low!"
   when guess.to_i == NUMBER
+    @background = "darkseagreen"
     "You got it right!"
   end
 end
